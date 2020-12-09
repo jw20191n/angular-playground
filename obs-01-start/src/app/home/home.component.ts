26c2e177f859c7def55440ba0b9f2c04396cd082
jw20191n <clairejw1015@gmail.com>
2020-12-08 20:28:06 -0500

    added custom observable in home component

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +14,22 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     //we get a value every second and we console log the value
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+
+    //create takes in a function
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(()=>{
+        observer.next(count); //we can call next() here to emit a new value
+        count++;
+      },1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     });
   }
 
