524d552d916a047619ab89a41bda9faa7532744c
jw20191n <clairejw1015@gmail.com>
2020-12-08 21:17:49 -0500

    replace eventemitter with subject

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
 
 @Component({
@@ -6,16 +7,21 @@ import { UserService } from './user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
       (didActivated) => {
         this.userActivated = didActivated;
       }
     );
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe();
   }
 }
