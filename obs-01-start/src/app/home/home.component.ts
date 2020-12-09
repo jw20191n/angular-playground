83c7d24bed5b2e37663e7a7aa0a1361b5c532394
jw20191n <clairejw1015@gmail.com>
2020-12-08 20:53:31 -0500

    added filter and map operator

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -34,7 +35,21 @@ export class HomeComponent implements OnInit, OnDestroy {
       },1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(
+    //pipe method is built into rxjs
+    // customIntervalObservable.pipe(map(
+    //   (data: number) => {
+    //     return 'Round: ' + (data + 1);
+    //   }
+    // ));
+
+    this.firstObsSubscription = customIntervalObservable.pipe(filter((data)=>{
+      return data > 0;
+    }),
+    map(
+      (data: number) => {
+        return 'Round: ' + (data + 1);
+      }
+    )).subscribe(
       data => {
         console.log(data);
     },
